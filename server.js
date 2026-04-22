226a1adc24771448f5e7217457cc5130ab272e10
unknown <Projjwal@alliancescitizens.com>
2026-04-22 12:18:00 +0530

    updated server origins

--- server.js
+++ server.js
@@ -25,7 +25,22 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://next.coolyourhome.in"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow Postman / curl
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 
 // Health check
